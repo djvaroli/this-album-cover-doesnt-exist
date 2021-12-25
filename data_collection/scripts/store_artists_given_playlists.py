0753f003799bdcc4ed7c53b3788966c3bb852718
Daniel Varoli <daniel.varoli@gmail.com>
2021-12-25 14:40:34 -0500

    refactor of data fetching

--- data_collection/scripts/store_artists_given_playlists.py
+++ data_collection/scripts/store_artists_given_playlists.py
@@ -5,7 +5,7 @@ artists that are present in the playlist. Then stores the artist data in a JSON 
 
 from tqdm import tqdm
 
-from data_collection import file_utils, spotify_utils
+from data_collection import utils, spotify_utils
 
 
 def get_artists_from_playlist(source_filepath: str = "data/playlists.json", out_filepath: str = "data/artists.json"):
@@ -15,14 +15,14 @@ def get_artists_from_playlist(source_filepath: str = "data/playlists.json", out_
     :param out_filepath:
     :return:
     """
-    playlists = file_utils.load_json(source_filepath)
+    playlists = utils.load_json(source_filepath)
     spotify_client = spotify_utils.get_spotify_client()
     artists = []
     for playlist in tqdm(playlists):
         playlist_artists = spotify_utils.get_playlist_artists(spotify_client, playlist["id"])
         artists.extend(playlist_artists)
 
-    file_utils.save_json(out_filepath, artists)
+    utils.save_json(out_filepath, artists)
 
 
 if __name__ == '__main__':
--- data_collection/scripts/store_track_data_given_albums.py
+++ data_collection/scripts/store_track_data_given_albums.py
@@ -5,13 +5,15 @@ Then stores the track data in a SQL table.
 
 from argparse import ArgumentParser
 from typing import List, Dict
+import random
 
 from tqdm import tqdm
 
 from data_collection.database import DB, SpotifyTrack, orm
-from data_collection import file_utils, spotify_utils
+from data_collection import utils, spotify_utils
 
 
+@orm.db_session
 def store_tracks_in_db(tracks: List[dict]):
     """
     Given a list of tracks stores them in the database.
@@ -28,6 +30,36 @@ def store_tracks_in_db(tracks: List[dict]):
             pass
 
 
+@orm.db_session
+def get_tracks_for_album_batch(album_batch: List[dict]) -> List[dict]:
+    """
+    Gets tracks for a batch of albums
+    :param album_batch:
+    :return:
+    """
+    album_batch_tracks = []
+
+    # renew the spotify client at every batch
+    spotify_client = spotify_utils.get_spotify_client(status_retries=5, requests_timeout=10)
+    for album in album_batch:
+
+        # first check if any tracks from the album are already in the database
+        if SpotifyTrack.is_album_in_table(album["id"]):
+            continue
+
+        album_cover_url = album["image_url"]
+        album_tracks: List[Dict] = spotify_utils.get_album_tracks(spotify_client, album["id"])
+
+        for track in album_tracks:
+            track["album_cover_url"] = album_cover_url
+            track["album_name"] = album["name"]
+            track["album_id"] = album["id"]
+
+        album_batch_tracks.extend(album_tracks)
+
+    return album_batch_tracks
+
+
 def store_track_data_given_albums(source_filepath: str):
     """
     Given a file with Spotify album data, extracts all individual tracks and stores
@@ -37,22 +69,13 @@ def store_track_data_given_albums(source_filepath: str):
     """
 
     # load the albums data from a json file
-    albums: List[Dict] = file_utils.load_json(source_filepath)
-    spotify_client = spotify_utils.get_spotify_client()
+    albums: List[Dict] = utils.load_json(source_filepath)
+    random.shuffle(albums)
+    album_batches = utils.list_into_batches(albums, batch_size=20)
 
-    for album in tqdm(albums, total=len(albums)):
-        with orm.db_session:
-            if SpotifyTrack.is_album_in_table(album["id"]):
-                continue
-
-            album_cover_url = album["image_url"]
-            album_tracks: List[Dict] = spotify_utils.get_album_tracks(spotify_client, album["id"])
-            for track in album_tracks:
-                track["album_cover_url"] = album_cover_url
-                track["album_name"] = album["name"]
-                track["album_id"] = album["id"]
-
-            store_tracks_in_db(album_tracks)
+    for album_batch in tqdm(album_batches, total=len(album_batches)):
+        album_batch_tracks = get_tracks_for_album_batch(album_batch)
+        store_tracks_in_db(album_batch_tracks)
 
 
 if __name__ == '__main__':
