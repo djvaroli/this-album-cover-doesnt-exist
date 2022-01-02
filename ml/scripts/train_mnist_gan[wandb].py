1d8fed698bfcb0f4596ee772b42e631e7744babb
Daniel Varoli <daniel.varoli@gmail.com>
2022-01-02 01:26:53 -0500

    [refactor]: Use correct function to rescale model output tensors for saving as image

--- ml/scripts/train_mnist_gan[wandb].py
+++ ml/scripts/train_mnist_gan[wandb].py
@@ -160,7 +160,7 @@ def train_gan():
             step_loss = train_step(context)
 
         model_prediction = context.generator_namespace.model(reference, training=False)
-        model_prediction = tf.cast(image_utils.arr_to_rgb_range(model_prediction), tf.int16)
+        model_prediction = tf.cast(image_utils.tensor_to_rgb_range(model_prediction), tf.int16)
         reference_images = image_utils.make_image_grid(model_prediction)
         reference_images = image_utils.array_to_image(reference_images)
 
